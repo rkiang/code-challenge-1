aed84c6306e19051f85eed118e538294d134da61
rkiang <ryanlkiang@gmail.com>
2017-07-31 13:28:54 -0500

    added click counter to generate button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var clicks = 0
 $(document).ready(function () {
     console.log('script.js is sourced in');
     $('#generate').on('click', function() {
+        clicks++;
         // console.log('generate was clicked');
-        $('body').append('<div></div>')
+        $('body').append('<div>' + '<p>This button was clicked ' + clicks + '</p>' + '</div>')
     })
 });
