927e258cc447c2d2dc9e24df12c6f202d3ee487c
rkiang <ryanlkiang@gmail.com>
2017-07-31 14:32:55 -0500

    attempted to return div to red, not working yet

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 var clicks = 0
 $(document).ready(function () {
     console.log('script.js is sourced in');
-    $('#generate').on('click', function() {
+    $('#generate').on('click', function () {
         clicks++;
         // console.log('generate was clicked');
         $('body').append('<div class="step2">' +
-        '<p>This button was clicked ' + clicks +
-        '</p>' + '<button class="swap">Swap</button>' +
-        '<button class="delete">Delete</button>' + '</div>')
+            '<p>This button was clicked ' + clicks +
+            '</p>' + '<button class="swap">Swap</button>' +
+            '<button class="delete">Delete</button>' + '</div>')
         // $('.step2').append('<button class="swap">Swap</button', '<button class="delete">Delete</button>')
-    
-    $('.delete').on('click', function() {
-        // console.log('delete was clicked');
-        
-    $(this).parent().remove()   
+
+        $('.delete').on('click', function () {
+            // console.log('delete was clicked');
+
+            $(this).parent().remove()
+        })
+        $('.step2').on('click', '.swap', function () {
+            // console.log('swap was clicked');
+            $(this).parent().css('background-color', 'yellow');
+
+        })
+        // $('.step2').on('click', '.swap', function() {
+        //     if ($(this).css('background-color') === 'yellow') {
+        //        $(this).parent().css('background-color', 'red'); 
+        //     } DID NOT WORK AS PLANNED
     })
-$('.step2').on('click', '.swap', function() {
-    // console.log('swap was clicked');
-    $(this).parent().css('background-color', 'yellow');
-    
-})
 });
-})
